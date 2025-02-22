e7d3ea09f8212350a0def3b9b0ef31f7b511181b
yactouat <yactouat@yactouat.com>
2025-02-22 19:31:36 +0100

    fixing empty plan bug in module 4

--- 4-plan-and-execute-pattern/app.py
+++ 4-plan-and-execute-pattern/app.py
@@ -1,54 +1,19 @@
-# plan and execute pattern
-
-![Plan and execute pattern](./plan-and-execute-pattern.png)
-
-This pattern aims at letting an agent:
-
-- come up with a multi-step plan to achieve a goal
-- execute the plan step by step
-- after each step, evaluate the results and adjust the plan as needed
-
-This pattern is more involved than ReAct patte, it outsources creating more complex logic to AI; however, it takes more time to execute.
-
----
-
-- prerequisites from `README.md`
-- `python3 -m venv venv && source venv/bin/activate`
-- create a `requirements.txt` file with the following content:
-
-```
-asyncio
-duckduckgo-search
-langchain
-langchain-community
-langchain-core
-langchain-google-genai
-langgraph
-pydantic
-python-dotenv
-```
-
-- `pip install -r requirements.txt`
-- we will now work in an `app.py` file, that is to be run from the folder of this module
-- `cp .env.example .env` and fill in your API key obtained from Google AI Studio
-
----
-
-## designing a web search tool
-
-### generating web search queries
-
-- this time, we are going to use an actual tool to spice things up, let's implement a basic web search functionality; first, we want to generate web search engine queries to maximize the chances of finding the right information:
-
-```python
 from dotenv import load_dotenv
 
 load_dotenv()
 
+import asyncio
 from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
+from langchain_core.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph import END, StateGraph, START
+from langgraph.prebuilt import create_react_agent
 from pydantic import BaseModel, Field
+import operator
+from typing import Annotated, List, Tuple, Union
+from typing_extensions import TypedDict
+
 
 plain_text_model = ChatGoogleGenerativeAI(
     max_tokens=None, model="gemini-2.0-flash", temperature=0, top_k=1
@@ -69,19 +34,6 @@ query_gen_prompt = PromptTemplate(
     template="""generate 3 web search engine queries to answer the query: {query}""",
 )
 query_gen_chain = query_gen_prompt | json_model
-
-queries_res = query_gen_chain.invoke(
-    {"query": "who are the top losers in the stock market today?"}
-)
-print(queries_res.queries)
-```
-
-### using DuckDuckGo search engine with the generated queries
-
-- with the queries generated, we can now build a chain to search the web and answer the initial user query:
-
-```python
-from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
 
 
 def search_the_web(query):
@@ -125,23 +77,8 @@ search_the_web_chain = (
     )
     | qa_chain
 )
-```
 
-- let's try the chain:
 
-```python
-res = search_the_web_chain.invoke(
-    {"query": "who are the top losers in the stock market today?"}
-)
-print(res)
-```
-
-- we can now turn this chain into a tool, to be used in a graph:
-
-```python
-from langchain_core.tools import tool
-
-# let's turn our web search into a tool that can be used by the agent
 @tool
 def search_the_web_tool(query: str) -> str:
     """
@@ -156,47 +93,15 @@ def search_the_web_tool(query: str) -> str:
     """
     res = search_the_web_chain.invoke({"query": query})
     return res.content
-```
 
----
-
-## designing an executor agent
-
-- the concept is pretty straightforward, the LLM that can call tools will be in charge of executing the plan:
-
-```python
-from langgraph.prebuilt import create_react_agent
 
 tools = [search_the_web_tool]
 
 executor = create_react_agent(
     plain_text_model,
     tools,
-    prompt="you are a helpful assistant equipped with a web search tool, you are to use this tool ONLY if necessary; otherwise, just answer the user query directly"
+    prompt="you are a helpful assistant equipped with a web search tool, you are to use this tool ONLY if necessary; otherwise, just answer the user query directly",
 )
-```
-
-- let's give it a spin:
-
-```python
-res = executor.invoke({
-    "messages": [
-        ("user", "what is current AAPL stock price?")
-    ]
-})
-print(res)
-```
-
----
-
-## focusing on the planning part
-
-- to track the execution of the plan, we need a state object:
-
-```python
-import operator
-from typing import Annotated, List, Tuple
-from typing_extensions import TypedDict
 
 
 # plan and execute requires to maintain a state of our flow, let's use a simple pre defined key-value store for that:
@@ -209,12 +114,7 @@ class PlanAndExecuteState(TypedDict):
     past_steps: Annotated[List[Tuple], operator.add]
     # final response of the agent
     response: str
-```
 
-- what we need now is a planner to plan steps based on a user query:
-
-```python
-from langchain_core.prompts import ChatPromptTemplate
 
 class Plan(BaseModel):
     """Plan to follow in the future"""
@@ -238,25 +138,6 @@ The result of the final step should be the final answer. Make sure that each ste
 planner = planner_prompt | ChatGoogleGenerativeAI(
     max_tokens=None, model="gemini-2.0-flash", temperature=0, top_k=1
 ).with_structured_output(Plan)
-```
-
-- let's test the planner:
-
-```python
-res = planner.invoke({
-    "messages": [("user", "help me build a balanced stocks portfolio focused on the technology sector")]
-})
-print(res)
-```
-
----
-
-## making the planning part more robust
-
-- let's create a "replanner", i.e. the layer that will re assess the plan after each executed step:
-
-```python
-from typing import Union
 
 
 class Response(BaseModel):
@@ -272,6 +153,7 @@ class Act(BaseModel):
         description="Action to perform. If you can respond to the user, even with a partial answer, use Response. "
         "If you need to further use tools to get the answer, use Plan."
     )
+
 
 replanner_prompt = ChatPromptTemplate.from_template(
     """For the given objective, update your plan based on the steps already completed.
@@ -304,25 +186,17 @@ replanner_prompt = ChatPromptTemplate.from_template(
 replanner = replanner_prompt | ChatGoogleGenerativeAI(
     max_tokens=None, model="gemini-2.0-flash", temperature=0, top_k=1
 ).with_structured_output(Act)
-```
 
----
-
-## putting it all together in a graph
-
-- first, write the nodes:
-
-```python
-from langgraph.graph import END
 
 def summarizer(state: PlanAndExecuteState):
     res = plain_text_model.invoke(
         [
             ("system", "You are a helpful assistant that summarizes content."),
-            ("user", str(state["past_steps"]))
+            ("user", str(state["past_steps"])),
         ]
     )
     return {"response": res.content}
+
 
 async def execute_step(state: PlanAndExecuteState):
     plan = state["plan"]
@@ -330,17 +204,17 @@ async def execute_step(state: PlanAndExecuteState):
     task = plan[0]
     task_formatted = f"""For the following plan:
 {plan_str}\n\nYou are tasked with executing step {1}, {task}."""
-    agent_response = await executor.ainvoke(
-        {"messages": [("user", task_formatted)]}
-    )
+    agent_response = await executor.ainvoke({"messages": [("user", task_formatted)]})
     return {
         # this will be concatenated to the `past_steps` list
         "past_steps": [(task, agent_response["messages"][-1].content)],
     }
 
+
 async def plan_step(state: PlanAndExecuteState):
     plan = await planner.ainvoke({"messages": [("user", state["input"])]})
     return {"plan": plan.steps}
+
 
 # we return either the final response or a new plan
 async def replan_step(state: PlanAndExecuteState):
@@ -349,18 +223,14 @@ async def replan_step(state: PlanAndExecuteState):
         return {"response": output.action.response}
     else:
         return {"plan": output.action.steps}
-    
+
+
 def should_end(state: PlanAndExecuteState):
     if state["plan"] == [] or "response" in state and state["response"]:
         return "summarizer"
     else:
         return "agent"
-```
 
-- we are now ready to define the edges and compile the graph:
-
-```python
-from langgraph.graph import StateGraph, START
 
 workflow = StateGraph(PlanAndExecuteState)
 
@@ -383,22 +253,19 @@ workflow.add_conditional_edges(
 workflow.add_edge("summarizer", END)
 
 graph = workflow.compile()
-```
 
-- let's try the whole thing and see the steps as they are executed:
-
-```python
-import asyncio
 
 async def main():
     # the max number of steps to take in the graph
     config = {"recursion_limit": 50}
-    inputs = {"input": "among recent top losers in the stock market, give me one that will probably go up again in the near future, don't overthink it; explain your reasoning"}
+    inputs = {
+        "input": "given the reaction to Covid19, how will gold price be affected in case of a new pandemic?"
+    }
     async for event in graph.astream(inputs, config=config):
         for k, v in event.items():
             if k != "__end__":
                 print(v)
 
+
 if __name__ == "__main__":
     asyncio.run(main())
-```
